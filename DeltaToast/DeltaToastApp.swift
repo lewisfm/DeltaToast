6be954b7154188cb1ecf6b96b64f7a20310234dc
Lewis McClelland <lewis@lewismcclelland.me>
2025-08-12 23:27:19 -0400

    Initial Commit

---
+++ DeltaToast/DeltaToastApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeltaToastApp.swift
+//  DeltaToast
+//
+//  Created by Lewis McClelland on 8/12/25.
+//
+
+import SwiftUI
+
+@main
+struct DeltaToastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DeltaToastTests/DeltaToastTests.swift
@@ -0,0 +1,17 @@
+//
+//  DeltaToastTests.swift
+//  DeltaToastTests
+//
+//  Created by Lewis McClelland on 8/12/25.
+//
+
+import Testing
+@testable import DeltaToast
+
+struct DeltaToastTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
